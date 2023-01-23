b010ea85bb998ac710988a5c95bb6eaeb605c941
Dmitry <k1791@mail.ru>
2023-01-23 22:23:54 +0500

    Funcs.py add function sub_number

--- funcs.py
+++ funcs.py
@@ -4,3 +4,10 @@ def sum_number(num_1,num_2):
   sum_num = num_1+num_2
 
   return sum_num
+
+def sub_number(num_1,num_2):
+
+  # находим разность 2 числа
+  sub_num = num_1-num_2
+
+  return sub_num
